1d03af0fa6e17414a6ccb1d88a213f709b03fe83
Pang Phanna <phanna.pang@gmail.com>
2019-05-21 14:30:04 +0700

    Add project files.

---
+++ AmazonUKTestDigital/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AmazonUKTestDigital")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AmazonUKTestDigital")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a9667a0-9412-45e6-a36a-9bd10f620e39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AmazonUKTestDigital/UnitTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AmazonUKTestDigital
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup Driver");
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.amazon.co.uk/";
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Console.WriteLine("Exit browser");
+            driver.Quit();
+        }
+
+        [Test]
+        public void SearchItem()
+        {
+            // Find Element search box and search text TShirt
+            IWebElement searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchBox.SendKeys("TShirt");
+            // Find Element search button and click to search
+            IWebElement searchButton = driver.FindElement(By.ClassName("nav-input"));
+            searchButton.Click();
+
+            // Wait finding Element result list
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2.0)).Until(ExpectedConditions.ElementExists(By.ClassName("s-result-list")));
+            IWebElement result = driver.FindElement(By.ClassName("s-result-list"));
+            Assert.NotNull(result, "Result list is found!");
+            Assert.IsTrue(result.Displayed);
+        }
+
+        [Test]
+        public void CreateAccount()
+        {
+            // Find Element signin button and click to redirect to signin form
+            IWebElement signinButton = driver.FindElement(By.CssSelector("#nav-tools>a"));
+            signinButton.Click();
+
+            // Wait finding Element create account and click to redirect to create account form
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2.0)).Until(ExpectedConditions.ElementExists(By.CssSelector(".a-button-inner>a")));
+            IWebElement createAccount = driver.FindElement(By.CssSelector(".a-button-inner>a"));
+            createAccount.Click();
+
+            /*
+             * Wait finding Element create account and click to redirect to create account form
+             * Fill form of creating account
+             */
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2.0)).Until(ExpectedConditions.ElementExists(By.ClassName("a-button-input")));
+            IWebElement name = driver.FindElement(By.Id("ap_customer_name"));
+            name.SendKeys("Phanna");
+            IWebElement email = driver.FindElement(By.Id("ap_email"));
+            email.SendKeys("pp@test.com");
+            IWebElement password = driver.FindElement(By.Id("ap_password"));
+            password.SendKeys("123456");
+            IWebElement repassword = driver.FindElement(By.Id("ap_password_check"));
+            repassword.SendKeys("123456");
+            IWebElement createAccountButton = driver.FindElement(By.ClassName("a-button-input"));
+
+            Assert.NotNull(createAccountButton, "Button submit element found!");
+            Assert.IsTrue(createAccountButton.Displayed);
+        }
+    }
+}
